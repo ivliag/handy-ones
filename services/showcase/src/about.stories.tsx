37095a102a1316fbfd05c7f34685ee28480d5058
Ivan Lyagushkin <javar@yandex-team.ru>
2022-10-12 14:05:40 +0100

    logo

---
+++ services/showcase/src/about.stories.tsx
@@ -0,0 +1,11 @@
+export const HandyOnes = () => (
+    <div>
+        <h1>This is Handy Ones</h1>
+        <p>Not yet a library, just a bunch of useful components to make your life easiar</p>
+        <ol>
+            <li><code className="ladle-code">handy-clamp</code> is a react-component to nicely cut extra text.</li>
+            <li><code className="ladle-code">handy-range-slider</code> is fully-customizable yet native range-slider React</li>
+            <li><code className="ladle-code">handy-svg</code> is a tool to properly inject SVG images to your web-project</li>
+        </ol>
+    </div>
+);
