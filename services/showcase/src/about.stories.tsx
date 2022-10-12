f102bb95d34cd15d30b0161e3a47af80abc26697
Ivan Lyagushkin <javar@yandex-team.ru>
2022-10-12 14:30:09 +0100

    about

--- services/showcase/src/about.stories.tsx
+++ services/showcase/src/about.stories.tsx
@@ -2,15 +2,14 @@ export const HandyOnes = () => (
     <div>
         <img
             src="https://raw.githubusercontent.com/ivliag/handy-ones/master/services/showcase/logo.png"
-            width={220}
-            height={220}
+            height={220 / 3 * 2}
         />
         <h1>This is Handy Ones</h1>
-        <p>Not yet a library, just a bunch of useful components to make your life easier</p>
+        <p>Not a library, but a bunch of useful tools to make your life easier</p>
         <ol>
-            <li><code className="ladle-code">handy-clamp</code> is a react-component to nicely cut extra text.</li>
-            <li><code className="ladle-code">handy-range-slider</code> is fully-customizable yet native range-slider React</li>
-            <li><code className="ladle-code">handy-svg</code> is a tool to properly inject SVG images to your web-project</li>
+            <li><code className="ladle-code">handy-clamp</code> is a React-component to nicely cut extra text</li>
+            <li><code className="ladle-code">handy-range-slider</code> is fully-customizable native range-slider for React</li>
+            <li><code className="ladle-code">handy-svg</code> is a tool to <a href="https://dev.to/javar/external-svgs-that-you-can-style-2a37" target="_blank" rel="noreferrer">properly inject SVG images</a> to your web-project</li>
         </ol>
     </div>
 );
