a5d28c392c59a0e3f94ae69d58e450983d46fbce
Ivan Liagushkin <ivan.liagushkin@usetwain.com>
2025-01-02 21:49:46 +0100

    Update about page with new packages

--- services/showcase/src/about.stories.tsx
+++ services/showcase/src/about.stories.tsx
@@ -11,6 +11,8 @@ export const HandyOnes = () => (
             <li><code className="ladle-code">handy-range-slider</code> is fully-customizable native range-slider for React</li>
             <li><code className="ladle-code">handy-scroll-strip</code> is a native scrollable panel with extra features</li>
             <li><code className="ladle-code">handy-svg</code> is a tool to <a href="https://dev.to/javar/external-svgs-that-you-can-style-2a37" target="_blank" rel="noreferrer">properly inject SVG images</a> to web pages</li>
+            <li><code className="ladle-code">handy-lazy-img</code> is a React component for intelligent image lazy-loading with responsive images support</li>
+            <li><code className="ladle-code">handy-copy-clipboard</code> is a flexible copy-to-clipboard component with visual state feedback</li>
         </ol>
         <p>Source code: <a href="https://github.com/ivliag/handy-ones">Github</a></p>
     </div>
