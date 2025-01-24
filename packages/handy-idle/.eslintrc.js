fee94e1e5769f88a1841097431d212db7d04eb35
Ivan Liagushkin <ivan.liagushkin@usetwain.com>
2025-01-24 10:11:26 +0100

    Add eslint config for handy-idle

---
+++ packages/handy-idle/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    root: true,
+    extends: ["@handy-ones/eslint-config/react-internal.js"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: true,
+    },
+};
